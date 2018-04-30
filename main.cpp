6b8661b94cb1e2f17e0e5b7b022233d2cab24e5a
Adam P. Goucher <apg44@cam.ac.uk>
2018-04-30 21:53:10 +0100

    Pull corrected lifelib

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 #include "lifelib/classifier.h"
 #include "lifelib/incubator.h"
 
-#define APG_VERSION "v4.24-" LIFELIB_VERSION
+#define APG_VERSION "v4.25-" LIFELIB_VERSION
 
 #include "includes/params.h"
 #include "includes/sha256.h"
