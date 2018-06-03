def0394386b9a5e6feb87bb76a6ffbb12ebff2a5
Adam P. Goucher <apg44@cam.ac.uk>
2018-06-03 17:34:45 +0100

    Isotropic Generations

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 #include "lifelib/classifier.h"
 #include "lifelib/incubator.h"
 
-#define APG_VERSION "v4.31-" LIFELIB_VERSION
+#define APG_VERSION "v4.32-" LIFELIB_VERSION
 
 #include "includes/params.h"
 #include "includes/sha256.h"
