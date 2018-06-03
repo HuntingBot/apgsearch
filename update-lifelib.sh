0c4d61c8e60e22d7376251e41fe49499853d4ad4
Adam P. Goucher <apg44@cam.ac.uk>
2018-06-03 14:39:00 +0100

    rule2asm has moved to rule3asm

--- update-lifelib.sh
+++ update-lifelib.sh
@@ -5,7 +5,7 @@ if [ -d "lifelib/avxlife" ]
 then
 printf "Ensuring lifelib is up-to-date...\n"
 rm -rf "lifelib/avxlife/lifelogic" | true
-python lifelib/avxlife/rule2asm.py "b3s23" > /dev/null
+python lifelib/rule3asm.py "b3s23" > /dev/null
 else
 printf "\033[33;1mDownloading lifelib...\033[0m\n"
 fi
