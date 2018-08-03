08b505460ef5860a74d9b379971ac7ac2760fc9c
Adam P. Goucher <goucher@statslab.cam.ac.uk>
2018-08-03 20:40:56 +0100

    Turtle stack

---
+++ coincludes/turtlestack.h
@@ -0,0 +1,41 @@
+#include <vector>
+
+namespace cgold {
+
+    template<typename T>
+    struct TurtleStack {
+        /*
+        * There are default elements all the way down...
+        */
+
+        public:
+        std::vector<T> stack;
+        T default_element;
+
+        TurtleStack(T de) {
+            default_element = de;
+        }
+
+        T operator[](size_t i) {
+            if (i < stack.size()) {
+                return stack[stack.size() - (1 + i)];
+            } else {
+                return default_element;
+            }
+        }
+
+        T pop() {
+            if (stack.empty()) {
+                return default_element;
+            } else {
+                T x = stack.back();
+                stack.pop_back();
+                return x;
+            }
+        }
+
+        void push_back(const T &x) { stack.push_back(x); }
+
+    };
+
+}
