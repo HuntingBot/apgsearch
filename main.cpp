af10d21625e4f097053c21da2c505cdeb3e58f3a
Adam P. Goucher <goucher@dpmms.cam.ac.uk>
2018-08-25 17:02:10 +0000

    AVX-512 performance increased from 5040 to 5400 soups/sec using better data structure

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include "lifelib/classifier.h"
 #include "lifelib/incubator.h"
 
-#define APG_VERSION "v4.5-" LIFELIB_VERSION
+#define APG_VERSION "v4.51-" LIFELIB_VERSION
 
 #include "includes/params.h"
 #include "includes/sha256.h"
