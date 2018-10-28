0eac231f3c9cf6b801ee239dbd660c7a614564e4
Adam P. Goucher <apg44@cam.ac.uk>
2018-10-28 22:18:17 +0000

    Increment version number

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include "lifelib/classifier.h"
 #include "lifelib/incubator.h"
 
-#define APG_VERSION "v4.53-" LIFELIB_VERSION
+#define APG_VERSION "v4.54-" LIFELIB_VERSION
 
 #include "includes/params.h"
 #include "includes/sha256.h"
