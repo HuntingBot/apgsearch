d83ac8acd4712347e1a688b3399f880df1c84327
Adam P. Goucher <apg44@cam.ac.uk>
2018-10-29 18:13:01 +0000

    BSFKL rulespace

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include "lifelib/classifier.h"
 #include "lifelib/incubator.h"
 
-#define APG_VERSION "v4.55-" LIFELIB_VERSION
+#define APG_VERSION "v4.56-" LIFELIB_VERSION
 
 #include "includes/params.h"
 #include "includes/sha256.h"
