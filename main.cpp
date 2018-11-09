58fa6c63542c686ac3b7df6cac28b52c23713724
Adam P. Goucher <apg44@cam.ac.uk>
2018-11-09 00:28:46 +0000

    Recursion bugfix

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include "lifelib/classifier.h"
 #include "lifelib/incubator.h"
 
-#define APG_VERSION "v4.63-" LIFELIB_VERSION
+#define APG_VERSION "v4.64-" LIFELIB_VERSION
 
 #include "includes/params.h"
 #include "includes/sha256.h"
