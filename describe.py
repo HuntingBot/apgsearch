ae0029e40575b2521738660a6d5858f68bff4ce0
Adam P. Goucher <apg44@cam.ac.uk>
2018-11-19 02:01:02 +0000

    Allow easier programmatic access to block data

--- describe.py
+++ describe.py
@@ -5,6 +5,8 @@ import hashlib
 import struct
 from datetime import datetime as dt
 
+from collections import OrderedDict
+
 '''
 We initially attempt to import sha3. On Python 2, this monkey-patches
 the hashlib library to contain an implementation of sha3_256 (Keccak),
@@ -68,17 +70,33 @@ def human_readable(data):
 
     return x;
 
+def block_rle(x):
 
-def dispblock(x):
+    pn = struct.unpack('<Q', x[64:72])[0]
+    seedroot = human_readable(x[32:64])
+    prevseed = seedroot + str(pn)
+
+    s  = '#C seed: %s\n' % prevseed
+    s += '#C sha2: %s\n' % sha2(prevseed.encode('utf8')).hexdigest()
+
+    digest = [(b if isinstance(b, int) else ord(b)) for b in sha2(prevseed.encode('utf8')).digest()]
+    bits = [((b >> (7-i)) & 1) for b in digest for i in range(8)]
+    rle = ''.join(['bo'[b] for b in bits])
+    rle = tuple([rle[i:16+i] for i in range(0, 256, 16)])
+    rle = '%s$%s$%s$%s$\n%s$%s$%s$%s$\n%s$%s$%s$%s$\n%s$%s$%s$%s!\n' % rle
+
+    s += 'x = 16, y = 16, rule = B3/S23\n'
+    s += rle
+
+    return s
+
+def block_info(x):
 
     bi = struct.unpack('<Q', x[80:88])[0]
-    
-    s = 'Block %d\n' % bi
-    s += ('=' * 64 + '\n\n')
+    od = OrderedDict()
+    od['idx'] = bi
 
-    s += 'Hexadecimal representation:\n'
-    s += '---------------------------\n'
-
+    od['hex'] = OrderedDict()
     l = ['prev_sha2', 'prev_sha3', 'misc_info', 'addr_full',
          'txmr_sha2', 'txmr_sha3', 'xtra_nonc', 'addr_mine']
 
@@ -91,50 +109,51 @@ def dispblock(x):
             # Python 2:
             h = ''.join("{:02x}".format(ord(c)) for c in x[32*i:32*(i+1)])
 
-        s += '%s|%s\n' % (h, r)
-
-    s += '\nMiscellaneous information:\n'
-    s +=    '-------------------------\n'
+        od['hex'][r] = h
 
     pn = struct.unpack('<Q', x[64:72])[0]
     ts = struct.unpack('<Q', x[72:80])[0]
-    ds = dt.utcfromtimestamp(ts//(10**9)).strftime('%Y-%m-%d %H:%M:%S')
+    ds = dt.utcfromtimestamp(ts//(10**9)).strftime('%Y-%m-%dT%H:%M:%S')
     ds += ('.' + str((10**9) + (ts % (10**9)))[1:])
 
-    s += 'prev_nonce: %d\n'      % pn
-    s += 'time_stamp: %d = %s\n' % (ts, ds)
-    s += 'blockindex: %d\n'      % bi
-    s += 'difficulty: %.4f\n'    % struct.unpack('<d', x[88:96])[0]
+    od['misc'] = OrderedDict()
+    od['misc']['prev_nonce'] = pn
+    od['misc']['time_stamp'] = ts
+    od['misc']['block_time'] = ds
+    od['misc']['blockindex'] = bi
+    od['misc']['difficulty'] = struct.unpack('<d', x[88:96])[0]
 
-    s += '\nPrevious soup:\n'
-    s +=    '-------------\n'
+    od['addresses'] = OrderedDict()
+    od['addresses']['addr_full'] = human_readable(x[96:128])
+    od['addresses']['addr_mine'] = human_readable(x[224:256])
 
-    seedroot = human_readable(x[32:64])
-    prevseed = seedroot + str(pn)
-
-    s += '#C seed: %s\n' % prevseed
-    s += '#C sha2: %s\n' % sha2(prevseed.encode('utf8')).hexdigest()
-
-    digest = [(b if isinstance(b, int) else ord(b)) for b in sha2(prevseed.encode('utf8')).digest()]
-    bits = [((b >> (7-i)) & 1) for b in digest for i in range(8)]
-    rle = ''.join(['bo'[b] for b in bits])
-    rle = tuple([rle[i:16+i] for i in range(0, 256, 16)])
-    rle = '%s$%s$%s$%s$\n%s$%s$%s$%s$\n%s$%s$%s$%s$\n%s$%s$%s$%s!\n' % rle
-
-    s += 'x = 16, y = 16, rule = B3/S23\n'
-    s += rle
-
-    s += '\nWinner addresses:\n'
-    s +=    '----------------\n'
-    s += 'addr_full: %s\n' % human_readable(x[96:128])
-    s += 'addr_mine: %s\n' % human_readable(x[224:256])
-
-    s += '\nBlock hashes:\n'
-    s +=    '------------\n'
-    s += '%s|%s\n' % (sha2(x).hexdigest(), 'this_sha2')
-
+    od['hashes'] = OrderedDict()
+    od['hashes']['this_sha2'] = sha2(x).hexdigest()
     if sha3 is not None:
-        s += '%s|%s\n' % (sha3(x).hexdigest(), 'this_sha3')
+        od['hashes']['this_sha3'] = sha3(x).hexdigest()
+
+    return od
+
+def display_block(x):
+
+    info = block_info(x)
+
+    s = 'Block %d\n' % info['idx']
+    s += ('=' * 64 + '\n\n')
+
+    for (t, k) in [('Hexadecimal representation', 'hex'),
+                   ('Miscellaneous information', 'misc'),
+                   ('Winner addresses', 'addresses'),
+                   ('Block hashes', 'hashes'),
+                   ('Previous soup', 'nonkey')]:
+
+        s += ('\n%s:\n' % t)
+        s += (('-' * len(t)) + '-\n')
+        if k in info:
+            for item in info[k].items():
+                s += '%s|%s\n' % item
+
+    s += block_rle(x)
 
     s += ('\n' + ('=' * 64) + '\n')
     return s
@@ -151,7 +170,7 @@ def main():
             x += bytes([0] * (256 - lr))
 
         for i in range(0, len(x), 256):
-            print(dispblock(x[i:i+256]))
+            print(display_block(x[i:i+256]))
 
 if __name__ == '__main__':
     main()
