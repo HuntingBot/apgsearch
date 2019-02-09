7db0c9ad8f1eba142adab6cdb7b2cbac9d72ae96
Adam P. Goucher <goucher@dpmms.cam.ac.uk>
2019-02-09 13:57:15 +0000

    Reorganise file structure

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include "lifelib/classifier.h"
 #include "lifelib/incubator.h"
 
-#define APG_VERSION "v4.93-" LIFELIB_VERSION
+#define APG_VERSION "v4.94-" LIFELIB_VERSION
 
 #include "includes/params.h"
 #include "includes/md5.h"
@@ -28,131 +28,10 @@
 #include "includes/verification.h"
 #include "includes/searching.h"
 
+#include "includes/apgluxe.h"
+
 int main (int argc, char *argv[]) {
 
-    if (apg::rule2int(RULESTRING) != 0) {
-        std::cerr << "Abort: apgsearch rule does not match lifelib rule" << std::endl;
-        return 1;
-    }
+    return run_apgluxe(argc, argv);
 
-    // Default values:
-    int soups_per_haul = 10000000;
-    std::string payoshaKey = "#anon";
-    std::string seed = reseed("original seed");
-    int parallelisation = 0;
-
-    #ifdef STDIN_SYM
-    int local_log = 1;
-    bool testing = true;
-    int verifications = 0;
-    #else
-    int local_log = 0;
-    bool testing = false;
-    int verifications = -1;
-    #endif
-
-    int nullargs = 1;
-    bool quitByUser = false;
-
-    int iterations = 0;
-
-    bool consumed_rule = false;
-    
-    // Extract options:
-    for (int i = 1; i < argc - 1; i++) {
-        if (strcmp(argv[i], "-k") == 0) {
-            payoshaKey = argv[i+1];
-        } else if (strcmp(argv[i], "-s") == 0) {
-            seed = argv[i+1];
-        } else if (strcmp(argv[i], "-n") == 0) {
-            soups_per_haul = atoi(argv[i+1]);
-        } else if (strcmp(argv[i], "-v") == 0) {
-            verifications = atoi(argv[i+1]);
-        } else if (strcmp(argv[i], "-i") == 0) {
-            iterations = atoi(argv[i+1]);
-        } else if (strcmp(argv[i], "-L") == 0) {
-            local_log = atoi(argv[i+1]);
-        } else if (strcmp(argv[i], "-t") == 0) {
-            testing = atoi(argv[i+1]);
-            if (testing) { iterations = 1; }
-        } else if (strcmp(argv[i], "-p") == 0) {
-            parallelisation = atoi(argv[i+1]);
-        } else if (strcmp(argv[i], "--rule") == 0) {
-            if (!consumed_rule) {
-                std::cout << "\033[1;33mapgluxe " << APG_VERSION << "\033[0m: ";
-                std::string desired_rulestring = argv[i+1];
-                if (strcmp(RULESTRING, argv[i+1]) == 0) {
-                    std::cout << "Rule \033[1;34m" << RULESTRING << "\033[0m is correctly configured." << std::endl;
-                } else {
-                    std::cout << "Rule \033[1;34m" << RULESTRING << "\033[0m does not match desired rule \033[1;34m";
-                    std::cout << desired_rulestring << "\033[0m." << std::endl;
-                    execvp("./recompile.sh", argv);
-                    return 1;
-                }
-            }
-            nullargs += 2;
-            consumed_rule = true;
-        } else if (strcmp(argv[i], "--symmetry") == 0) {
-            std::cout << "\033[1;33mapgluxe " << APG_VERSION << "\033[0m: ";
-            std::string desired_symmetry = argv[i+1];
-            if (strcmp(SYMMETRY, argv[i+1]) == 0) {
-                std::cout << "Symmetry \033[1;34m" << SYMMETRY << "\033[0m is correctly configured." << std::endl;
-                nullargs += 2;
-            } else {
-                std::cout << "Symmetry \033[1;34m" << SYMMETRY << "\033[0m does not match desired symmetry \033[1;34m";
-                std::cout << desired_symmetry << "\033[0m." << std::endl;
-                execvp("./recompile.sh", argv);
-                return 1;
-            }
-        }
-    }
-
-    if(strcmp(SYMMETRY, "stdin") == 0) parallelisation = 0;
-    
-    if ((argc == nullargs) && (argc > 1)) { return 0; }
-
-    // Disable verification by default if running on a HPC;
-    // otherwise verify three hauls per uploaded haul:
-    if (verifications < 0) {
-        verifications = (parallelisation <= 4) ? 5 : 0;
-    }
-    
-    std::cout << "\nGreetings, this is \033[1;33mapgluxe " << APG_VERSION;
-    std::cout << "\033[0m, configured for \033[1;34m" << RULESTRING << "/";
-    std::cout << SYMMETRY << "\033[0m.\n" << std::endl;
-
-    std::cout << "\033[32;1mLifelib version:\033[0m " << LIFELIB_VERSION << std::endl;
-    std::cout << "\033[32;1mCompiler version:\033[0m " << __VERSION__ << std::endl;
-    std::cout << "\033[32;1mPython version:\033[0m " << PYTHON_VERSION << std::endl;
-
-    std::cout << std::endl;
-
-    while (!quitByUser) {
-        if (verifications > 0) {
-            std::cout << "Peer-reviewing hauls:\n" << std::endl;
-            // Verify some hauls:
-            for (int j = 0; j < verifications; j++) {
-                bool earlyquit = verifySearch(payoshaKey);
-                if (earlyquit) { break; }
-            }
-            std::cout << "\nPeer-review complete; proceeding search.\n" << std::endl;
-        }
-
-        // Run the search:
-        std::cout << "Using seed " << seed << std::endl;
-        if (parallelisation > 0) {
-            quitByUser = parallelSearch(soups_per_haul, parallelisation, payoshaKey, seed, local_log);
-        } else {
-            quitByUser = runSearch(soups_per_haul, payoshaKey, seed, local_log, testing);
-        }
-        seed = reseed(seed);
-        std::cout << "New seed: " << seed << "; iterations = " << iterations << "; quitByUser = " << quitByUser << std::endl;
-
-        iterations -= 1;
-        if (iterations == 0) { break; }
-    }
-
-    std::cout << "Terminating..." << std::endl;
-
-    return 0;
 }
